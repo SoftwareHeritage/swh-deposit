17b83de2cfd8dfc73e66fb07028b9415a5991c24
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-07-28 16:33:40 +0200

    views: Add a basic view

--- swh/urls.py
+++ swh/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from swh.deposit.views import hello
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/', hello)
 ]
