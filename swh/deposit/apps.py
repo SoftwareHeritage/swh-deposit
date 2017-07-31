4e141e9e79ca482d8c0aca247e5d83bf2b96d5c9
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-07-31 12:47:03 +0200

    views: Add basic views to list clients

--- swh/deposit/apps.py
+++ swh/deposit/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class DepositConfig(AppConfig):
-    name = 'deposit'
+    name = 'swh.deposit'
--- swh/deposit/views.py
+++ swh/deposit/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Client
 
 
-def hello(request):
-    return HttpResponse("Hello")
+def index(request):
+    return HttpResponse('SWH Deposit API - WIP')
+
+
+def clients(request):
+    """List existing clients.
+
+    """
+    cs = Client.objects.all()
+
+    return HttpResponse('Clients: %s' % ','.join((str(c) for c in cs)))
+
+
+def client(request, client_id):
+    """List information about one client.
+
+    """
+    c = Client.objects.filter(pk=client_id).all()
+    if len(c) <= 0:
+        raise Http404('Client with id %s not found' % client_id)
+
+    c = c[0]
+    return HttpResponse('Client {id: %s, name: %s}' % (c.id, c.name))
