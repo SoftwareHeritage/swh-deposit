118bee4bb002b80e94db20e5b1a628b9222d55b8
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-08-01 11:10:30 +0200

    swh.wsgi: Adapt application endpoint for deployment

--- swh/wsgi.py
+++ swh/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swh.settings")
+sys.path.append('/etc/softwareheritage')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deposit.settings")
 
 application = get_wsgi_application()
