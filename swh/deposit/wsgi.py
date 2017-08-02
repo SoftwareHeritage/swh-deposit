613fbb11b7206a5efff71eb44ea456b77010479e
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-08-02 13:42:32 +0200

    views: Add missing license headers

--- swh/deposit/wsgi.py
+++ swh/deposit/wsgi.py
@@ -1,5 +1,10 @@
+# Copyright (C) 2017  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
 """
-WSGI config for swh project.
+WSGI config for swh.deposit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
