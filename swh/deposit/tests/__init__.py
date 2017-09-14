84ba6accc6df283dbe8226e9abcb917d53d135a3
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-09-14 17:32:41 +0200

    swh.deposit.tests: Inhibit configuration loading from disk
    And mock the objstorage

--- swh/deposit/tests/__init__.py
+++ swh/deposit/tests/__init__.py
@@ -6,6 +6,38 @@
 import os
 import django
 
+from swh.objstorage import _STORAGE_CLASSES
+
+try:
+    from .objstorage import MockObjStorage
+    _STORAGE_CLASSES['mock'] = MockObjStorage
+except ImportError:
+    raise ValueError('Development error - '
+                     'Problem during setup for mock objstorage')
+
+TEST_CONFIG = {
+    'objstorage': {
+        'cls': 'mock',
+        'args': {}
+    },
+    'max_upload_size': 209715200,
+    'verbose': False,
+    'noop': False,
+}
+
+
+def parse_config_file(base_filename=None, config_filename=None,
+                      additional_configs=None, global_config=True):
+    return TEST_CONFIG
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swh.web.settings.development")
+
+from swh.deposit.api.deposit import SWHDeposit  # noqa
+from swh.deposit.api.service_document import SWHServiceDocument  # noqa
+
+# monkey patch :\
+SWHServiceDocument.parse_config_file = parse_config_file
+SWHDeposit.parse_config_file = parse_config_file
+
 django.setup()
