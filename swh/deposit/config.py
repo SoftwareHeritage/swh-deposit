a22792ae2f4bf865aff0ddc1c0c75480acc406a4
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-10-02 17:02:59 +0200

    swh.deposit.scheduler.cli: Add a scheduling implementation on deposit
    WIP, this still needs:
    - integration testing (manual so far)
    - clean up routine
    - scheduler api interface (for now this uses celery directly)

--- swh/deposit/config.py
+++ swh/deposit/config.py
@@ -3,6 +3,7 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+import os
 import logging
 
 from swh.core.config import SWHConfig
@@ -14,6 +15,35 @@ CONT_FILE_IRI = 'cont_file_iri'
 SD_IRI = 'servicedocument'
 COL_IRI = 'upload'
 STATE_IRI = 'status'
+
+
+AUTHORIZED_PLATFORMS = ['development', 'production']
+
+
+def setup_django_for(platform):
+    """Setup function for command line tools (swh.deposit.create_user,
+       swh.deposit.scheduler.cli) to initialize the needed db access.
+
+    Note:
+        Do not import any django related module prior to this function
+        call. Otherwise, this will raise an
+        django.core.exceptions.ImproperlyConfigured error message.
+
+    Args:
+        platform (str): the platform the scheduling is running
+
+    Raises:
+        ValueError in case of wrong platform inputs.
+
+    """
+    if platform not in AUTHORIZED_PLATFORMS:
+        raise ValueError('Platform should be one of %s' % AUTHORIZED_PLATFORMS)
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'swh.deposit.settings.%s' % platform)
+
+    import django
+    django.setup()
 
 
 class SWHDefaultConfig(SWHConfig):
