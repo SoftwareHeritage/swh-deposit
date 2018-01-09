69040f8814eedce2f881200222ae81225836040c
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-01-09 16:16:26 +0100

    swh.deposit: Add splash screen to homepage
    Closes T915

--- swh/deposit/urls.py
+++ swh/deposit/urls.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017  The Software Heritage developers
+# Copyright (C) 2017-2018  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -28,18 +28,23 @@ Examples:
 """
 
 from django.conf.urls import url, include
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.base import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
 
+favicon_view = RedirectView.as_view(url='/static/img/icons/swh-logo-32x32.png',
+                                    permanent=True)
 
-def index(req):
-    return HttpResponse('SWH Deposit API')
+
+def default_view(req):
+    return render(req, "homepage.html")
 
 
 urlpatterns = [
-    url(r'^$', index, name='home'),
+    url(r'^favicon\.ico$', favicon_view),
     url(r'^1/', include('swh.deposit.api.urls')),
     url(r'^1/private/', include('swh.deposit.api.private.urls')),
+    url(r'^$', default_view, name='home'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
