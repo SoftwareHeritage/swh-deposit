0de54036cf29ffc71db8c2a58d2b74ff5097c30e
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-09-28 15:15:09 +0200

    docs/Makefile: Excluding deposit settings from autodoc generation
    autodoc tries to import discovered modules independently from their
    runtime use.
    
    As swh/deposit/settings holds multiple runtime settings (including
    production), this wrongly tries to load the production one.
    
    This fixes all html documentation generation.
    
    Close T1182
    Close D447

--- docs/conf.py
+++ docs/conf.py
@@ -5,9 +5,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "swh.deposit.settings.development")
 django.setup()
 
-from swh.docs.sphinx.conf import *  # noqa
-from swh.docs.sphinx.conf import autodoc_mock_imports
-
-autodoc_mock_imports += [
-    'swh.deposit.settings',
-]
+from swh.docs.sphinx.conf import *  # NoQA
