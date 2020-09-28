87b39c10eea2ae2b4714e330a1b6ca104a3fcf81
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-09-28 13:57:05 +0200

    test_deposit_atom: Fix incomplete tests
    They were all an instance of the same missing SLUG header instead of their
    dedicated perimeters...

--- swh/deposit/tests/api/test_deposit_atom.py
+++ swh/deposit/tests/api/test_deposit_atom.py
@@ -54,8 +54,10 @@ def test_post_deposit_atom_400_with_empty_body(
         reverse(COL_IRI, args=[deposit_collection.name]),
         content_type="application/atom+xml;type=entry",
         data=atom_dataset["entry-data-empty-body"],
+        HTTP_SLUG="external-id",
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert b"Empty body request is not supported" in response.content
 
 
 def test_post_deposit_atom_400_badly_formatted_atom(
@@ -68,8 +70,10 @@ def test_post_deposit_atom_400_badly_formatted_atom(
         reverse(COL_IRI, args=[deposit_collection.name]),
         content_type="application/atom+xml;type=entry",
         data=atom_dataset["entry-data-badly-formatted"],
+        HTTP_SLUG="external-id",
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert b"Malformed xml metadata" in response.content
 
 
 def test_post_deposit_atom_parsing_error(
@@ -82,8 +86,10 @@ def test_post_deposit_atom_parsing_error(
         reverse(COL_IRI, args=[deposit_collection.name]),
         content_type="application/atom+xml;type=entry",
         data=atom_dataset["entry-data-parsing-error-prone"],
+        HTTP_SLUG="external-id",
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert b"Malformed xml metadata" in response.content
 
 
 def test_post_deposit_atom_no_slug_header(
@@ -122,6 +128,7 @@ def test_post_deposit_atom_unknown_collection(authenticated_client, atom_dataset
         HTTP_SLUG="something",
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert b"Unknown collection" in response.content
 
 
 def test_post_deposit_atom_entry_initial(
