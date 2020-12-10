SoftwareHeritage/swh-deposit

commit a8e86a92eeeba4ed5114ff2ef4096ba25213d98a

Explicitly use the atom: prefix internally.
This mostly does not change the protocol used (except in the error messages),
it's just an internal change for the server and for the client.

The only change in the protocol is that local tags (eg. `<entry>...</entry>`)
are no longer assumed to be in the Atom namespace (like
`<entry xmlns="http://www.w3.org/2005/Atom">...</entry>`), but they
never should have been in the first place.

Default namespaces / unprefixed tags are a footgun because it's too
easy to add tags in the default namespace without noticing,
or use the wrong namespace.

swh/deposit/api/checks.py
@@ -30,11 +30,11 @@ def check_metadata(metadata: Dict) -> Tuple[bool, Optional[Dict]]:
     """
     # following fields are mandatory
     required_fields = {
-        "author": False,
+        "atom:author": False,
     }
     # at least one value per couple below is mandatory
     alternate_fields = {
-        ("name", "title"): False,
+        ("atom:name", "atom:title"): False,
     }
 
     for field, value in metadata.items():

swh/deposit/tests/api/test_checks.py
@@ -12,16 +12,16 @@ from swh.deposit.api.checks import check_metadata
     "metadata_ok",
     [
         {
-            "url": "something",
-            "external_identifier": "something-else",
-            "name": "foo",
-            "author": "someone",
+            "atom:url": "something",
+            "atom:external_identifier": "something-else",
+            "atom:name": "foo",
+            "atom:author": "someone",
         },
         {
-            "url": "some url",
-            "external_identifier": "some id",
-            "title": "bar",
-            "author": "no one",
+            "atom:url": "some url",
+            "atom:external_identifier": "some id",
+            "atom:title": "bar",
+            "atom:author": "no one",
         },
     ],
 )
@@ -36,22 +36,22 @@ def test_api_checks_check_metadata_ok(metadata_ok, swh_checks_deposit):
     [
         (
             {
-                "url": "something",
-                "external_identifier": "something-else",
-                "author": "someone",
+                "atom:url": "something",
+                "atom:external_identifier": "something-else",
+                "atom:author": "someone",
             },
             {
                 "summary": "Mandatory alternate fields are missing",
-                "fields": ["name or title"],
+                "fields": ["atom:name or atom:title"],
             },
         ),
         (
             {
-                "url": "something",
-                "external_identifier": "something-else",
-                "title": "foobar",
+                "atom:url": "something",
+                "atom:external_identifier": "something-else",
+                "atom:title": "foobar",
             },
-            {"summary": "Mandatory fields are missing", "fields": ["author"],},
+            {"summary": "Mandatory fields are missing", "fields": ["atom:author"],},
         ),
     ],
 )

swh/deposit/tests/api/test_deposit_private_check.py
@@ -133,10 +133,10 @@ def test_deposit_ko_unsupported_tarball(
         assert len(details["metadata"]) == 2
         mandatory = details["metadata"][0]
         assert mandatory["summary"] == MANDATORY_FIELDS_MISSING
-        assert set(mandatory["fields"]) == set(["author"])
+        assert set(mandatory["fields"]) == set(["atom:author"])
         alternate = details["metadata"][1]
         assert alternate["summary"] == ALTERNATE_FIELDS_MISSING
-        assert alternate["fields"] == ["name or title"]
+        assert alternate["fields"] == ["atom:name or atom:title"]
 
         deposit = Deposit.objects.get(pk=deposit.id)
         assert deposit.status == DEPOSIT_STATUS_REJECTED

swh/deposit/tests/api/test_deposit_update.py
@@ -362,7 +362,7 @@ def test_add_metadata_to_unknown_deposit(
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
     response_content = parse_xml(response.content)
-    assert "Unknown collection name" in response_content["sword:error"]["summary"]
+    assert "Unknown collection name" in response_content["sword:error"]["atom:summary"]
 
 
 def test_add_metadata_to_unknown_collection(
@@ -386,7 +386,7 @@ def test_add_metadata_to_unknown_collection(
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
     response_content = parse_xml(response.content)
-    assert "Unknown collection name" in response_content["sword:error"]["summary"]
+    assert "Unknown collection name" in response_content["sword:error"]["atom:summary"]
 
 
 def test_replace_metadata_to_unknown_deposit(
@@ -410,7 +410,7 @@ def test_replace_metadata_to_unknown_deposit(
     response_content = parse_xml(response.content)
     assert (
         "Deposit %s does not exist" % unknown_deposit_id
-        == response_content["sword:error"]["summary"]
+        == response_content["sword:error"]["atom:summary"]
     )
 
 
@@ -434,7 +434,7 @@ def test_add_archive_to_unknown_deposit(
     response_content = parse_xml(response.content)
     assert (
         "Deposit %s does not exist" % unknown_deposit_id
-        == response_content["sword:error"]["summary"]
+        == response_content["sword:error"]["atom:summary"]
     )
 
 
@@ -458,7 +458,7 @@ def test_replace_archive_to_unknown_deposit(
     response_content = parse_xml(response.content)
     assert (
         "Deposit %s does not exist" % unknown_deposit_id
-        == response_content["sword:error"]["summary"]
+        == response_content["sword:error"]["atom:summary"]
     )
 
 
@@ -751,17 +751,16 @@ def test_put_update_metadata_done_deposit_failure_empty_xml(
     """
     update_uri = reverse(EDIT_IRI, args=[deposit_collection.name, complete_deposit.id])
 
-    for atom_key in ["entry-data-empty-body", "entry-data-empty-body-no-namespace"]:
-        atom_content = atom_dataset[atom_key]
-        response = authenticated_client.put(
-            update_uri,
-            content_type="application/atom+xml;type=entry",
-            data=atom_content,
-            HTTP_X_CHECK_SWHID=complete_deposit.swhid,
-        )
+    atom_content = atom_dataset["entry-data-empty-body"]
+    response = authenticated_client.put(
+        update_uri,
+        content_type="application/atom+xml;type=entry",
+        data=atom_content,
+        HTTP_X_CHECK_SWHID=complete_deposit.swhid,
+    )
 
-        assert response.status_code == status.HTTP_400_BAD_REQUEST
-        assert b"Empty body request is not supported" in response.content
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert b"Empty body request is not supported" in response.content
 
 
 def test_put_update_metadata_done_deposit_failure_functional_checks(
@@ -790,7 +789,8 @@ def test_put_update_metadata_done_deposit_failure_functional_checks(
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert b"Functional metadata checks failure" in response.content
     # detail on the errors
-    assert b"- Mandatory fields are missing (author)" in response.content
+    assert b"- Mandatory fields are missing (atom:author)" in response.content
     assert (
-        b"- Mandatory alternate fields are missing (name or title)" in response.content
+        b"- Mandatory alternate fields are missing (atom:name or atom:title)"
+        in response.content
     )

swh/deposit/cli/client.py
@@ -95,7 +95,7 @@ def generate_metadata(
 
     # generate a metadata file with the minimum required metadata
     codemetadata = {
-        "entry": {
+        "atom:entry": {
             "@xmlns:atom": "http://www.w3.org/2005/Atom",
             "@xmlns:codemeta": "https://doi.org/10.5063/SCHEMA/CODEMETA-2.0",
             "codemeta:identifier": external_id,

swh/deposit/tests/api/test_collection_post_atom.py
@@ -52,16 +52,15 @@ def test_post_deposit_atom_400_with_empty_body(
     """Posting empty body request should return a 400 response
 
     """
-    for atom_key in ["entry-data-empty-body", "entry-data-empty-body-no-namespace"]:
-        atom_content = atom_dataset[atom_key]
-        response = authenticated_client.post(
-            reverse(COL_IRI, args=[deposit_collection.name]),
-            content_type="application/atom+xml;type=entry",
-            data=atom_content,
-            HTTP_SLUG="external-id",
-        )
-        assert response.status_code == status.HTTP_400_BAD_REQUEST
-        assert b"Empty body request is not supported" in response.content
+    atom_content = atom_dataset["entry-data-empty-body"]
+    response = authenticated_client.post(
+        reverse(COL_IRI, args=[deposit_collection.name]),
+        content_type="application/atom+xml;type=entry",
+        data=atom_content,
+        HTTP_SLUG="external-id",
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert b"Empty body request is not supported" in response.content
 
 
 def test_post_deposit_atom_400_badly_formatted_atom(
@@ -297,7 +296,7 @@ def test_post_deposit_atom_entry_multiple_steps(
         "utf-8"
     )  # noqa
 
-    for link in response_content["link"]:
+    for link in response_content["atom:link"]:
         if link["@rel"] == "http://purl.org/net/sword/terms/add":
             se_iri = link["@href"]
             break

swh/deposit/tests/api/test_collection_post_binary.py
@@ -125,9 +125,9 @@ def test_post_deposit_binary_upload_ok(
     assert response_content["swh:deposit_status"] == deposit.status
 
     # deprecated tags
-    assert response_content["deposit_archive"] == sample_archive["name"]
-    assert int(response_content["swh:deposit_id"]) == deposit.id
-    assert response_content["deposit_status"] == deposit.status
+    assert response_content["atom:deposit_archive"] == sample_archive["name"]
+    assert int(response_content["atom:deposit_id"]) == deposit.id
+    assert response_content["atom:deposit_status"] == deposit.status
 
     edit_iri = reverse("edit_iri", args=[deposit_collection.name, deposit.id])
 

swh/deposit/tests/api/test_collection_post_multipart.py
@@ -119,7 +119,7 @@ def test_post_deposit_multipart_zip(
             assert deposit_request.raw_metadata is None
         else:
             assert (
-                deposit_request.metadata["id"]
+                deposit_request.metadata["atom:id"]
                 == "urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a"
             )
             assert deposit_request.raw_metadata == data_atom_entry
@@ -189,7 +189,7 @@ def test_post_deposit_multipart_tar(
             assert deposit_request.raw_metadata is None
         else:
             assert (
-                deposit_request.metadata["id"]
+                deposit_request.metadata["atom:id"]
                 == "urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a"
             )
             assert deposit_request.raw_metadata == data_atom_entry
@@ -258,7 +258,7 @@ def test_post_deposit_multipart_put_to_replace_metadata(
             check_archive(sample_archive["name"], deposit_request.archive.name)
         else:
             assert (
-                deposit_request.metadata["id"]
+                deposit_request.metadata["atom:id"]
                 == "urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a"
             )
             assert deposit_request.raw_metadata == data_atom_entry
@@ -288,7 +288,7 @@ def test_post_deposit_multipart_put_to_replace_metadata(
             check_archive(sample_archive["name"], deposit_request.archive.name)
         else:
             assert (
-                deposit_request.metadata["id"]
+                deposit_request.metadata["atom:id"]
                 == "urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a"
             )
             assert (

commit 00795b41d6ab75a2d9d79c61eaea9ca70e0617ce

Accept <codemeta:name> and <codemeta:author> as alternatives to <atom:name>/<atom:title> and <atom:author>.
This was broken by a8e86a92eeeba4ed5114ff2ef4096ba25213d98a,
as the check_metadata() checks whether there is a tag that *contains*
the expected name, checking for 'author' (the old name of 'atom:author')
accidentally matched 'codemeta:author' as well.

This resulted in the right behavior in the majority of the cases
(accepted 'codemeta:author'), but for the wrong reason, and explicitly
renaming to atom:author broke this.

Ditto for name.

A future commit will remove the substring matching to remove
false positives (eg. 'atom:authorblahblah' should not be accepted
as 'atom:author')

swh/deposit/api/checks.py
@@ -14,7 +14,6 @@ Mandatory fields:
 from typing import Dict, Optional, Tuple
 
 MANDATORY_FIELDS_MISSING = "Mandatory fields are missing"
-ALTERNATE_FIELDS_MISSING = "Mandatory alternate fields are missing"
 
 
 def check_metadata(metadata: Dict) -> Tuple[bool, Optional[Dict]]:
@@ -28,36 +27,22 @@ def check_metadata(metadata: Dict) -> Tuple[bool, Optional[Dict]]:
           ok (False, <detailed-error>) otherwise.
 
     """
-    # following fields are mandatory
-    required_fields = {
-        "atom:author": False,
-    }
     # at least one value per couple below is mandatory
     alternate_fields = {
-        ("atom:name", "atom:title"): False,
+        ("atom:name", "atom:title", "codemeta:name"): False,
+        ("atom:author", "codemeta:author"): False,
     }
 
     for field, value in metadata.items():
-        for name in required_fields:
-            if name in field:
-                required_fields[name] = True
-
         for possible_names in alternate_fields:
             for possible_name in possible_names:
                 if possible_name in field:
                     alternate_fields[possible_names] = True
                     continue
 
-    mandatory_result = [k for k, v in required_fields.items() if not v]
-    optional_result = [" or ".join(k) for k, v in alternate_fields.items() if not v]
+    mandatory_result = [" or ".join(k) for k, v in alternate_fields.items() if not v]
 
-    if mandatory_result == [] and optional_result == []:
+    if mandatory_result == []:
         return True, None
-    detail = []
-    if mandatory_result != []:
-        detail.append({"summary": MANDATORY_FIELDS_MISSING, "fields": mandatory_result})
-    if optional_result != []:
-        detail.append(
-            {"summary": ALTERNATE_FIELDS_MISSING, "fields": optional_result,}
-        )
+    detail = [{"summary": MANDATORY_FIELDS_MISSING, "fields": mandatory_result}]
     return False, {"metadata": detail}

swh/deposit/tests/api/test_checks.py
@@ -23,11 +23,12 @@ from swh.deposit.api.checks import check_metadata
             "atom:title": "bar",
             "atom:author": "no one",
         },
+        {"atom:url": "some url", "codemeta:name": "bar", "codemeta:author": "no one",},
     ],
 )
 def test_api_checks_check_metadata_ok(metadata_ok, swh_checks_deposit):
     actual_check, detail = check_metadata(metadata_ok)
-    assert actual_check is True
+    assert actual_check is True, detail
     assert detail is None
 
 
@@ -41,8 +42,8 @@ def test_api_checks_check_metadata_ok(metadata_ok, swh_checks_deposit):
                 "atom:author": "someone",
             },
             {
-                "summary": "Mandatory alternate fields are missing",
-                "fields": ["atom:name or atom:title"],
+                "summary": "Mandatory fields are missing",
+                "fields": ["atom:name or atom:title or codemeta:name"],
             },
         ),
         (
@@ -51,7 +52,34 @@ def test_api_checks_check_metadata_ok(metadata_ok, swh_checks_deposit):
                 "atom:external_identifier": "something-else",
                 "atom:title": "foobar",
             },
-            {"summary": "Mandatory fields are missing", "fields": ["atom:author"],},
+            {
+                "summary": "Mandatory fields are missing",
+                "fields": ["atom:author or codemeta:author"],
+            },
+        ),
+        (
+            {
+                "atom:url": "something",
+                "atom:external_identifier": "something-else",
+                "codemeta:title": "bar",
+                "atom:author": "someone",
+            },
+            {
+                "summary": "Mandatory fields are missing",
+                "fields": ["atom:name or atom:title or codemeta:name"],
+            },
+        ),
+        (
+            {
+                "atom:url": "something",
+                "atom:external_identifier": "something-else",
+                "atom:title": "foobar",
+                "author": "foo",
+            },
+            {
+                "summary": "Mandatory fields are missing",
+                "fields": ["atom:author or codemeta:author"],
+            },
         ),
     ],
 )

swh/deposit/tests/api/test_deposit_private_check.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 import pytest
 from rest_framework import status
 
-from swh.deposit.api.checks import ALTERNATE_FIELDS_MISSING, MANDATORY_FIELDS_MISSING
+from swh.deposit.api.checks import MANDATORY_FIELDS_MISSING
 from swh.deposit.api.private.deposit_check import (
     MANDATORY_ARCHIVE_INVALID,
     MANDATORY_ARCHIVE_MISSING,
@@ -130,13 +130,15 @@ def test_deposit_ko_unsupported_tarball(
         assert len(details["archive"]) == 1
         assert details["archive"][0]["summary"] == MANDATORY_ARCHIVE_UNSUPPORTED
         # metadata check failure
-        assert len(details["metadata"]) == 2
+        assert len(details["metadata"]) == 1
         mandatory = details["metadata"][0]
         assert mandatory["summary"] == MANDATORY_FIELDS_MISSING
-        assert set(mandatory["fields"]) == set(["atom:author"])
-        alternate = details["metadata"][1]
-        assert alternate["summary"] == ALTERNATE_FIELDS_MISSING
-        assert alternate["fields"] == ["atom:name or atom:title"]
+        assert set(mandatory["fields"]) == set(
+            [
+                "atom:author or codemeta:author",
+                "atom:name or atom:title or codemeta:name",
+            ]
+        )
 
         deposit = Deposit.objects.get(pk=deposit.id)
         assert deposit.status == DEPOSIT_STATUS_REJECTED

swh/deposit/tests/api/test_deposit_update.py
@@ -796,11 +796,12 @@ def test_put_update_metadata_done_deposit_failure_functional_checks(
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert b"Functional metadata checks failure" in response.content
     # detail on the errors
-    assert b"- Mandatory fields are missing (atom:author)" in response.content
-    assert (
-        b"- Mandatory alternate fields are missing (atom:name or atom:title)"
-        in response.content
+    msg = (
+        b"- Mandatory fields are missing ("
+        b"atom:name or atom:title or codemeta:name, "
+        b"atom:author or codemeta:author)"
     )
+    assert msg in response.content
 
 
 def test_put_atom_with_create_origin_and_external_identifier(