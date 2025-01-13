e308a110c86602bf1433b2e623fc8aeff9518338
Antoine Lambert <anlambert@softwareheritage.org>
2025-01-13 16:40:24 +0100

    test_gunicorn_config: Fix tests after swh-core v3.7.0 release

--- swh/deposit/tests/test_gunicorn_config.py
+++ swh/deposit/tests/test_gunicorn_config.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019-2024  The Software Heritage developers
+# Copyright (C) 2019-2025  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -22,7 +22,8 @@ def test_post_fork_default(mocker):
         integrations=[django_integration],
         environment=None,
         debug=False,
-        release=None,
+        release="0.0.0",
+        traces_sample_rate=None,
     )
 
 
@@ -46,6 +47,7 @@ def test_post_fork_with_dsn_env(mocker):
         environment="test",
         debug=False,
         release=None,
+        traces_sample_rate=None,
     )
 
 
@@ -70,4 +72,5 @@ def test_post_fork_debug(mocker):
         environment="test",
         debug=True,
         release=None,
+        traces_sample_rate=None,
     )
