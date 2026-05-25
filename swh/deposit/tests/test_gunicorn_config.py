6343e9e8d2054aed7a7254903064c9647f00709a
Antoine Lambert <anlambert@softwareheritage.org>
2026-05-25 14:02:30 +0200

    test_gunicorn_config: Fix failures since swh-core v4.6.2 release

--- swh/deposit/tests/test_gunicorn_config.py
+++ swh/deposit/tests/test_gunicorn_config.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019-2025  The Software Heritage developers
+# Copyright (C) 2019-2026  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -24,6 +24,7 @@ def test_post_fork_default(mocker):
         debug=False,
         release="0.0.0",
         traces_sample_rate=None,
+        send_default_pii=False,
     )
 
 
@@ -48,6 +49,7 @@ def test_post_fork_with_dsn_env(mocker):
         debug=False,
         release=None,
         traces_sample_rate=None,
+        send_default_pii=False,
     )
 
 
@@ -73,4 +75,5 @@ def test_post_fork_debug(mocker):
         debug=True,
         release=None,
         traces_sample_rate=None,
+        send_default_pii=False,
     )
